61011c02ec46b7e619e53c893ba80e5c9b4fbf0c
Karthi <karthiraja268@gmail.com>
2021-09-12 17:54:23 +0530

    authenticate removed

--- routes/users.js
+++ routes/users.js
@@ -4,8 +4,8 @@ const userModule = require('../modules/UserModule');
 const LoginModule = require('../modules/LoginModule');
 const Authenticate = require('../Authenticate');
 
-router.post('/saveuser', [Authenticate.Authenticate], userModule.postUser);
-router.get('/getusers', [Authenticate.Authenticate], userModule.getUser);
+router.post('/saveuser', userModule.postUser);
+router.get('/getusers', userModule.getUser);
 router.get('/getusers/:id', userModule.getUserbyid);
 router.post('/login', LoginModule.loginUser);
 module.exports = router;
