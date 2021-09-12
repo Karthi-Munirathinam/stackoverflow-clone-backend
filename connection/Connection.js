a047e593b57ab6847581ad9289dc27fd0049bd1e
Karthi <karthiraja268@gmail.com>
2021-09-12 13:41:32 +0530

    mongodb added

--- connection/Connection.js
+++ connection/Connection.js
@@ -1,7 +1,9 @@
+const dotenv = require('dotenv');
+dotenv.config();
 exports.connect = () => {
     try {
         const mongoose = require('mongoose');
-        mongoose.connect('mongodb://localhost:27017/stackoverflow-clone', { useNewUrlParser: true, useUnifiedTopology: true })
+        mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 
     } catch (error) {
         console.log(error);
