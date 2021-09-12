f541a63995d2801d20b60fc6a9bf248ad3602f49
Karthi <karthiraja268@gmail.com>
2021-09-12 18:00:22 +0530

    auth removed

--- routes/questions.js
+++ routes/questions.js
@@ -4,7 +4,7 @@ const questionsmodule = require('../modules/QuestionModule');
 const getTags = require('../modules/Tagsmodule')
 const Authenticate = require('../Authenticate');
 
-router.post('/postquestion', [Authenticate.Authenticate], questionsmodule.postQuestions);
+router.post('/postquestion', questionsmodule.postQuestions);
 router.put('/postanswer/:id', questionsmodule.putanswer);
 router.get('/getquestion', questionsmodule.getQuestions);
 router.get('/getquestion/:id', questionsmodule.getQuestion);
